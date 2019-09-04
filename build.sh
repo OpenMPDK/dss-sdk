35df765b56814ce7c5e4c81c9191377e216cd448
somnathr <som.roy@samsung.com>
2019-09-04 11:51:05 -0700

    Modified build.sh to install dependant module

--- build.sh
+++ build.sh
@@ -20,6 +20,13 @@ CWD="$(pwd)"
 CWDNAME=`basename "$CWD"`
 OD="${CWD}/../${CWDNAME}_out"
 
+if [ $(yum list installed | cut -f1 -d" " | grep --extended '^boost-devel' | wc -l) -eq 1 ]; then
+  echo "boost-devel already installed";
+else
+  echo "yum install boost-devel"
+  yum install boost-devel
+fi
+
 rm -rf $OD
 mkdir $OD
 
