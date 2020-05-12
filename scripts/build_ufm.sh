f8c4a7c4399970689b219feccc5415b5a5c44a9e
Matthew Durand <matt.d@samsung.com>
2020-05-12 15:58:53 -0700

    Add comments, echo output and read versions from build_env

--- scripts/build_ufm.sh
+++ scripts/build_ufm.sh
@@ -1,20 +1,31 @@
 #! /usr/bin/bash
 set -e
 
-UFM_VER="${1:-0}"
-UFM_BROKER_VER="${2:-0}"
-
 script_dir=$(readlink -f "$(dirname "$0")")
-top_dir=${script_dir}/../
+top_dir=${script_dir}/..
 
+# Read build environment vars
+pushd "${script_dir}"
+# shellcheck disable=SC1091
+. ./build_env
+
+# Clean existing RPM/DEB packages
+echo "Removing existing RPM/DEB packages"
+rm -f "${top_dir}"/ansible/release/*.deb
+rm -f "${top_dir}"/ansible/release/*.rpm
+
+# Build Fabric Manager
+echo "Building Fabric Mananger"
 pushd "${top_dir}/ufm/fabricmanager"
-./makeufmpackage.sh -prJ "${UFM_VER}"
+./makeufmpackage.sh -prJ "$UFM_VER"
 cp ./*.rpm "${top_dir}"/ansible/release/
 cp ./*.deb "${top_dir}"/ansible/release/
 popd
 
+# Build Message Broker
+echo "Building Message Broker"
 pushd "${top_dir}/ufm/ufm_msg_broker"
-./makebrokerpackage.sh -prJ "${UFM_BROKER_VER}"
+./makebrokerpackage.sh -prJ "$UFM_BROKER_VER"
 cp ./*.rpm "${top_dir}"/ansible/release/
 cp ./*.deb "${top_dir}"/ansible/release/
 popd
