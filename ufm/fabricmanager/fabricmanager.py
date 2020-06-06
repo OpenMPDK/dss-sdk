bb8c355ec707118cbdba2e0d924bd36cbb130243
Lu Fan <lu1.fan@samsung.com>
2020-06-06 09:11:37 -0700

    Merge conflicts

--- ufm/fabricmanager/fabricmanager.py
+++ ufm/fabricmanager/fabricmanager.py
@@ -44,7 +44,7 @@ from rest_api.redfish.Drive import DriveCollectionEmulationAPI, DriveEmulationAP
 from rest_api.redfish.EthernetInterface import EthernetInterfaceCollectionEmulationAPI, EthernetInterfaceEmulationAPI
 
 from rest_api.redfish.Fabric_api import FabricCollectionAPI, FabricAPI
-from rest_api.redfish.Switch import SwitchCollection, SwitchApi
+from rest_api.redfish.Switch import SwitchCollection, Switch
 from rest_api.redfish.Port import PortCollection, Port
 from rest_api.redfish.VLAN import VLANCollection, VLAN
 
@@ -55,7 +55,8 @@ from systems import port_def
 from systems.ufm.ufm import Ufm
 from systems.ufmarg import UfmArg
 
-from systems.switch.switch import Switch
+from systems.switch.switch import EthSwitch
+from systems.switch.switch_arg import SwitchArg
 
 from systems.smart.smart import Smart
 
@@ -225,7 +226,7 @@ elif (MODE is not None and MODE.lower() == 'local'):
     api.add_resource(FabricAPI, REST_BASE + 'Fabrics/<string:ident>')
     api.add_resource(SwitchCollection, REST_BASE + 'Fabrics/<string:ident>/Switches',
                      resource_class_kwargs={'rest_base': REST_BASE, 'suffix': 'Fabrics'})
-    api.add_resource(SwitchApi, REST_BASE + 'Fabrics/<string:ident1>/Switches/<string:ident2>',
+    api.add_resource(Switch, REST_BASE + 'Fabrics/<string:ident1>/Switches/<string:ident2>',
                      resource_class_kwargs={'rest_base': REST_BASE})
     api.add_resource(PortCollection, REST_BASE + 'Fabrics/<string:ident1>/Switches/<string:ident2>/Ports',
                      resource_class_kwargs={'rest_base': REST_BASE, 'suffix': 'Fabrics'})
@@ -285,11 +286,11 @@ def readConfigDataFromFile(filename):
     configData = dict()
     if os.path.isfile(installPath + filename):
         with open(installPath + filename) as f:
-            configData = yaml.load(f)
+            configData = yaml.safe_load(f)
     else:
         if os.path.isfile(filename):
             with open(filename) as f:
-                configData = yaml.load(f)
+                configData = yaml.safe_load(f)
         else:
             log.error("Failed to open config file ufm.yaml")
             sys.exit(-1)
@@ -338,9 +339,13 @@ def initializeSubSystems(subSystems, ufmArg, ufmMetadata):
         pass
 
     try:
-        if ufmMetadata['switch']['enable']:
-            ufmArg.switchConfig=ufmMetadata['switch']
-            subSystems.append( Switch() )
+        for switch_arg in ufmMetadata['switch']:
+            if switch_arg['enable']:
+                swArg = SwitchArg(sw_type = switch_arg['sw_type'],
+                                  sw_ip = switch_arg['sw_ip'],
+                                  log = ufmArg.log,
+                                  db = ufmArg.db)
+                subSystems.append( EthSwitch(swArg) )
     except:
         pass
 
