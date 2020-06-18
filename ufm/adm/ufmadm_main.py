7099cd68a44ed5ef56d8e055c4a90df45d1753a4
Tom Rainey <tom.rainey@ssi.samsung.com>
2020-06-17 17:23:12 -0700

    added (get only) fabric, switch, port, and vlans to cli interface

--- ufm/adm/ufmadm_main.py
+++ ufm/adm/ufmadm_main.py
@@ -2,16 +2,20 @@
 
 from ufmmenu import UfmMenu
 
+
 class MainMenu(UfmMenu):
     def __init__(self):
-        UfmMenu.__init__(self, title="Samsung UFM Managment Utility", \
-            usage=self._cli_usage)
+        UfmMenu.__init__(self, title="Samsung UFM Managment Utility",
+                         usage=self._cli_usage)
 
-        self.add_item(labels=["sys", "sy"], action=self._sys_action, \
-            desc="Storage System Management")
+        self.add_item(labels=["sys", "sy"], action=self._sys_action,
+                      desc="Storage System Management")
 
-        self.add_item(labels=["ufm", "uf"], action=self._ufm_action, \
-            desc="UFM System Management")
+        self.add_item(labels=["ufm", "uf"], action=self._ufm_action,
+                      desc="UFM System Management")
+
+        self.add_item(labels=["fab", "fb"], action=self._fab_action,
+                      desc="Fabrics Management")
         return
 
     def _sys_action(self, menu, item):
@@ -28,6 +32,13 @@ class MainMenu(UfmMenu):
         self.set_menu(ufm_menu)
         return
 
+    def _fab_action(self, menu, item):
+        from ufmadm_fabrics import FabricsMenu
+
+        fab_menu = FabricsMenu()
+        self.set_menu(fab_menu)
+        return
+
     def _cli_usage(self, menu, item):
         prog = menu.name + ".py"
         print()
@@ -35,11 +46,11 @@ class MainMenu(UfmMenu):
         print()
         print("This program can be run in two modes: MENU mode or CLI mode.")
         print()
-        print("1) MENU Mode:") 
+        print("1) MENU Mode:")
         print()
         print("   USAGE: python "+prog)
         print()
-        print("2) CLI Mode:") 
+        print("2) CLI Mode:")
         print()
         print("   USAGE: python "+prog+" <command list>")
         print()
@@ -51,4 +62,3 @@ class MainMenu(UfmMenu):
         print("     Example: python "+prog+" sys 1 stor 0 1")
         print()
         return
-
---
+++ ufm/adm/ufmadm_switches.py
@@ -0,0 +1,107 @@
+from ufmmenu import UfmMenu
+import ufmapi
+
+
+class SwitchesMenu(UfmMenu):
+    def __init__(self, fab=""):
+        UfmMenu.__init__(self, name="sws", back_func=self._back_action)
+
+        rsp = ufmapi.redfish_get("/Fabrics/" + fab + "/Switches")
+
+        if rsp is None:
+            return
+
+        if "Members" not in rsp:
+            return
+
+        count = 0
+        print()
+        print("*  Switches Collection:")
+
+        for member in rsp["Members"]:
+            sw = member["@odata.id"].split("/")[6]
+            print("      Switch: ("+str(count)+")", sw)
+
+            self.add_item(labels=[str(count)],
+                          action=self._menu_action, priv=sw, desc=sw)
+
+            count = count + 1
+
+        self.fab = fab
+        return
+
+    def _back_action(self, menu, item):
+        from ufmadm_fabrics import FabricMenu
+
+        fabric_menu = FabricMenu(self.fab)
+        self.set_menu(fabric_menu)
+        return
+
+    def _menu_action(self, menu, item):
+        sw_menu = SwitchMenu(fab=self.fab, sw=item.priv)
+        self.set_menu(sw_menu)
+        return
+
+
+class SwitchMenu(UfmMenu):
+    def __init__(self, fab="", sw=""):
+        if sw is None:
+            print("ERROR: Null Switch provided.")
+            return
+
+        if fab is None:
+            print("ERROR: Null Fabric provided.")
+            return
+
+        print("fab: " + fab + ", sw: " + sw)
+        UfmMenu.__init__(self, name="sw", back_func=self._back_action)
+
+        rsp = ufmapi.redfish_get("/Fabrics/" + fab + "/Switches/" + sw)
+
+        if rsp is None:
+            return
+
+        if "SerialNumber" not in rsp:
+            return
+
+        print()
+        print("*        Fabric:", fab)
+        print("*        Switch:", sw)
+        print("  Serial Number:", rsp["SerialNumber"])
+        print("          Model:", rsp["Model"])
+        print("           UUID:", rsp["UUID"])
+
+        if len(rsp["Ports"]) > 0:
+            print("    Ports: Present")
+            self.add_item(labels=["pts", "pts"],
+                          action=self._ports_action,
+                          desc="Ports")
+
+        if len(rsp["VLANs"]) > 0:
+            print("    VLANs: Present")
+            self.add_item(labels=["vlans", "vlans"],
+                          action=self._vlans_action,
+                          desc="VLANs")
+
+        self.fab = fab
+        self.sw = sw
+        return
+
+    def _back_action(self, menu, item):
+        switches_menu = SwitchesMenu(fab=self.fab)
+        self.set_menu(switches_menu)
+        return
+
+    def _ports_action(self, menu, item):
+        from ufmadm_ports import PortsMenu
+
+        ports_menu = PortsMenu(fab=self.fab, sw=self.sw)
+        self.set_menu(ports_menu)
+        return
+
+    def _vlans_action(self, menu, item):
+        from ufmadm_vlans import VlansMenu
+
+        vlans_menu = VlansMenu(fab=self.fab, sw=self.sw)
+        self.set_menu(vlans_menu)
+        return
