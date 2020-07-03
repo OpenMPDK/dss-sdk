464366919c199aa25fa40615ec3fe4bf204456c5
Ramaraj Pandian <nr.pandian@samsung.com>
2020-07-02 19:00:16 -0700

    changed install dir to nkv-target, added ustat

--- target/build.sh
+++ target/build.sh
@@ -2,12 +2,12 @@
 
 set -o xtrace
 
-CWD="$(pwd)"
+target_dir=$(readlink -f "$(dirname "$0")")
 
 if [ "$#" -ne 1 ]; then
-    TGT_VER="0.5.0"
+    TARGET_VER="0.5.0"
 else
-    TGT_VER="$1"
+    TARGET_VER="$1"
 fi
 TGT_HASH=`git rev-parse --verify HEAD`
 sed -i -e "s/^\#define OSS_TARGET_VER.\+$/#define OSS_TARGET_VER \"$TGT_VER\"/" include/version.h
@@ -15,17 +15,46 @@ sed -i -e "s/^\#define OSS_TARGET_HASH.\+$/#define OSS_TARGET_HASH \"$TGT_HASH\"
 
 #source ~/.dragonfly
 
-CWDNAME=`basename "$CWD"`
-#OD="${CWD}/${CWDNAME}_out"
-OD="${CWD}/../df_out"
+CWDNAME=`basename "${target_dir}"`
+build_dir="${target_dir}/../df_out"
 
-rm -rf $OD
+rm -rf $build_dir
 
-cd ./oss
+cd $target_dir/oss
 ./apply-patch.sh
 
-mkdir $OD
-cd ${OD}
+mkdir $build_dir
+mkdir $build_dir/rpm-build
+cd ${build_dir}
 
-cmake $CWD -DCMAKE_BUILD_TYPE=Debug
+cmake $target_dir -DCMAKE_BUILD_TYPE=Debug
 make spdk_tcp
+
+mkdir -p "${build_dir}"/rpm-build/SRPMS
+mkdir -p "${build_dir}"/rpm-build/SPECS
+mkdir -p "${build_dir}"/rpm-build/RPMS/x86_64
+mkdir -p "${build_dir}"/rpm-build/BUILD/nkv-target
+mkdir -p "${build_dir}"/rpm-build/BUILDROOT
+mkdir -p "${build_dir}"/rpm-build/BUILD/nkv-target/usr/dss/
+
+
+#mkdir -p "${build_dir}"/rpm-build/BUILD/nkv-target/etc/systemd/system
+#cp -rf "${target_dir}"/scripts/*.service "${build_dir}"/rpm-build/BUILD/nkv-target/etc/systemd/system/
+
+mkdir -p ""${build_dir}"/rpm-build/BUILD/nkv-target"/etc/rsyslog.d
+cat > ""${build_dir}"/rpm-build/BUILD/nkv-target"/etc/rsyslog.d/dfly.conf << EOF
+if \$programname == 'dfly' or \$syslogtag == '[dfly]:' \\
+then -/var/log/dragonfly/dfly.log
+&  stop
+EOF
+
+cp "${target_dir}"/scripts/genrpm.sh "${build_dir}"/rpm-build/SPECS/
+cp -rf ${build_dir}/nkv-target "${build_dir}"/rpm-build/BUILD/nkv-target/usr/dss/
+
+if ! sh "${build_dir}"/rpm-build/SPECS/genrpm.sh "${build_dir}/rpm-build" "$TARGET_VER"
+then
+	echo "nkv-target RPM creation failed"
+	exit
+fi
+
+cp "${build_dir}"/rpm-build/RPMS/x86_64/*.rpm "${build_dir}"/
