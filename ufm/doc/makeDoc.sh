718ceca9486c665b5341a5d163e2718ecf62d00e
Egil Rasmussen <e.rasmussen@samsung.com>
2020-07-13 13:21:50 -0700

    [FAB-415] Add feature to generate documentation
     - Add documentation
     - Reduced the number of file for the documentation

---
+++ ufm/doc/makeDoc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#
+mkdir -p source/_static
+mkdir -p source/_templates/
+
+make clean
+
+# build html
+make html
+
+# build pdf from html
+# sphinx-build -b rinoh source/ buildpdf/rinoh
+
