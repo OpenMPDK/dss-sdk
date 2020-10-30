e7a822cd45fde95b49685a80ce3203eb607a76bc
Asim Salam <asim.salam@samsung.com>
2020-10-30 10:18:43 -0700

    Fix the code to return the IP address of the default interface and not the IP address of the default gateway itself

--- ufm/fabricmanager/systems/nkv/nkv_monitor.py
+++ ufm/fabricmanager/systems/nkv/nkv_monitor.py
@@ -150,15 +150,12 @@ class NkvMonitor(object):
          Return the first valid nic ip
          else return (127.0.0.1)
         '''
-        # for ifaceName in interfaces():
-        #     addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': 'No IP addr'}])]
-        #     if ifaceName != 'lo':
-        #         # print('{}: {}'.format(ifaceName, ', '.join(addresses)) )
-        #         return addresses[0]
-        #
-        # return "127.0.0.1"
+
         gways = gateways()
-        return gways['default'][AF_INET][0]
+        default_ifc = gways['default'][AF_INET][1]
+        default_ipaddr = ifaddresses(default_ifc)[AF_INET][0]['addr']
+
+        return default_ipaddr if default_ipaddr is not None else 'No IP addr'
 
     def get_host_ip(self):
         try:
