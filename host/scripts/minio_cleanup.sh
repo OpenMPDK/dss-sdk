aedb282fbf1b0bb48db9c8d703a6b844e0fa9bcf
Harsh Roogi <h.roogi@samsung.com>
2021-07-15 16:32:13 -0700

    Changes to minio_cleanup for custom nkv_config.json file

--- host/scripts/minio_cleanup.sh
+++ host/scripts/minio_cleanup.sh
@@ -1,11 +1,12 @@
 export LD_LIBRARY_PATH=../lib
 start_drive=$1
 end_drive=$2
-[ $# -lt 2 ] && { echo "Usage: $0 <start_drive> <end_drive>"; exit 1; }
-for i in $(eval echo {$1..$2});
+conf_file=$3
+[ $# -lt 2 ] && { echo "Usage: $0 <start_drive> <end_drive> <nkv_config.json>"; exit 1; }
+for i in $(eval echo {$start_drive..$end_drive});
 do 
-    ./nkv_test_cli -c ../conf/nkv_config.json -i msl-ssg-dl04 -p 1030 -w meta/.minio.sys/format.json -s /dev/nvme${i}n1 -o 2 -v 2097152
-    ./nkv_test_cli -c ../conf/nkv_config.json -i msl-ssg-dl04 -p 1030 -w meta/.minio.sys/kv-volumes -s /dev/nvme${i}n1 -o 2 -v 2097152
-    ./nkv_test_cli -c ../conf/nkv_config.json -i msl-ssg-dl04 -p 1030 -w meta/.minio.sys/config/config.json/xl.json -s /dev/nvme${i}n1 -o 2 -v 2097152
+    ./nkv_test_cli -c ../conf/$conf_file -i msl-ssg-dl04 -p 1030 -w meta/.minio.sys/format.json -s /dev/nvme${i}n1 -o 2 -v 2097152
+    ./nkv_test_cli -c ../conf/$conf_file -i msl-ssg-dl04 -p 1030 -w meta/.minio.sys/kv-volumes -s /dev/nvme${i}n1 -o 2 -v 2097152
+    ./nkv_test_cli -c ../conf/$conf_file -i msl-ssg-dl04 -p 1030 -w meta/.minio.sys/config/config.json/xl.json -s /dev/nvme${i}n1 -o 2 -v 2097152
 done
 
