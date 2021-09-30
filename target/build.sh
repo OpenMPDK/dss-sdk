e2361bda0946020aafaf2a9b9078439fecc26e3b
Matt Durand <matt.d@samsung.com>
2021-09-30 16:20:21 -0700

    Validate GCC version range

--- target/build.sh
+++ target/build.sh
@@ -3,22 +3,81 @@
 #
 # set -o xtrace
 
+# Minimum and Maximum GCC versions
+GCCMINVER=5.1.0
+GCCMAXVER=5.5.0
+
+# GCC Setenv scripts for CentOS
+GCCSETENV=/usr/local/bin/setenv-for-gcc510.sh
+GCCRESTORE=/usr/local/bin/restore-default-paths-gcc510.sh
+
 die()
 {
     echo "$*"
     exit 1
 }
 
-GCCERRMSG="Build/install GCC 5.1.0 RPM from gcc-builder: https://github.com/BobSteagall/gcc-builder"
-GCCSETENV=/usr/local/bin/setenv-for-gcc510.sh
-GCCRESTORE=/usr/local/bin/restore-default-paths-gcc510.sh
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+testvercomp () {
+    vercomp $1 $2
+    case $? in
+        0) op='=';;
+        1) op='>';;
+        2) op='<';;
+    esac
+    if [[ $op != $3 ]]
+    then
+        return 1
+    else
+        return 0
+    fi
+}
 
 # Load GCC 5.1.0 paths
 if test -f "$GCCSETENV"
 then
     source $GCCSETENV
+fi
+
+# Check gcc version
+GCCVER=$(gcc --version | grep -oP '^gcc \([^)]+\) \K[^ ]+')
+
+# Validate GCC version is supported
+if testvercomp $GCCVER $GCCMINVER '<' || testvercomp $GCCVER $GCCMAXVER '>'
+then
+    die "ERROR - Found GCC version: $GCCVER. Must be between $GCCMINVER and $GCCMAXVER."
 else
-    die "GCC 5.1.0 RPM is not installed. ${GCCERRMSG}"
+    echo "Supported GCC version found: $GCCVER"
 fi
 
 target_dir=$(readlink -f "$(dirname "$0")")
