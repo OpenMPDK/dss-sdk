22af21fd3f01f6c11fcb9a4b931eb50dd8a471c8
cliang01.li <cliang01.li@samsung.com>
2023-12-24 18:50:45 -0800

    Add unittests for dss_host.py and dss_target.py

---
+++ unittests/test_dss_host.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+
+"""
+# The Clear BSD License
+#
+# Copyright (c) 2023 Samsung Electronics Co., Ltd.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted (subject to the limitations in the disclaimer
+# below) provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Samsung Electronics Co., Ltd. nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+# THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from host.scripts.dss_host import (
+    is_ipv4, getSubnet, get_ip_port_nqn_info, decode,
+    build_driver, install_kernel_driver, get_ips_for_vlan,
+    drive_to_addr_map, mountpt_to_nqn_addr_map, subnet_drive_map,
+    discover_dist, config_minio_dist, config_minio_sa, dss_host_args
+)
+import pytest
+import secrets
+import os
+from conftest import (
+    mock_exec_cmd, mock_os_dirname_host,
+    mock_exec_cmd_remote,
+    MockArgParser
+)
+
+
+@pytest.mark.usefixtures(
+    "get_sample_ipv4_addr",
+    "get_sample_ipv6_addr",
+    "get_sample_nvme_discover_output"
+)
+class TestDSSHost():
+    def test_is_ipv4(self, get_sample_ipv4_addr, get_sample_ipv6_addr):
+        assert bool(is_ipv4(get_sample_ipv4_addr))
+        assert not is_ipv4(get_sample_ipv6_addr)
+
+    def test_getSubnet(self, get_sample_ipv4_addr):
+        addr = get_sample_ipv4_addr
+        subnet = '.'.join(addr.split('.')[0:3])
+        assert subnet == getSubnet(addr)
+
+    def test_get_ip_port_nqn_info(self, get_sample_nvme_discover_output):
+        data = get_ip_port_nqn_info(get_sample_nvme_discover_output, "rdma")
+        proto = 'rdma'
+        port = '1024'
+        subsystem = 'nqn.2023-06.io:msl-dpe-perf35-kv_data1'
+        ip = 'x.x.x.x'
+        assert data == [[proto, port, subsystem, ip]]
+
+    def test_decode(self):
+        bytes = bytearray(secrets.randbits(8) for _ in range(200))
+        assert isinstance(decode(bytes), str)
+
+    def test_build_driver(self, mocker):
+        mocker.patch("host.scripts.dss_host.exec_cmd", mock_exec_cmd)
+        mocker.patch("os.path.dirname", mock_os_dirname_host)
+        build_driver()
+
+    def test_install_kernel_driver(self, mocker):
+        cwd = os.getcwd()
+        mocker.patch("host.scripts.dss_host.exec_cmd", mock_exec_cmd)
+        mocker.patch("os.path.dirname", mock_os_dirname_host)
+        install_kernel_driver(512)
+
+    def test_get_ips_for_vlan(self, mocker):
+        mocker.patch("host.scripts.dss_host.exec_cmd_remote",
+                     mock_exec_cmd_remote)
+        res = get_ips_for_vlan("1234", "host", ["pw"])
+        assert len(res) > 0
+
+    def test_drive_to_addr_map(self, mocker):
+        mocker.patch("host.scripts.dss_host.exec_cmd", mock_exec_cmd)
+        res = drive_to_addr_map()
+        assert len(res) == 4
+
+    def test_mountpt_to_nqn_addr_map(self, mocker):
+        mocker.patch("host.scripts.dss_host.exec_cmd", mock_exec_cmd)
+        res = mountpt_to_nqn_addr_map()
+        assert len(res) == 4
+        for dev in res.keys():
+            assert "nqn" in res[dev]
+            assert "addr" in res[dev]
+
+    def test_subnet_drive_map(self, mocker):
+        mocker.patch("host.scripts.dss_host.exec_cmd", mock_exec_cmd)
+        res = subnet_drive_map()
+        assert len(res) > 0
+
+    def test_discover_dist(self, mocker):
+        mocker.patch("host.scripts.dss_host.exec_cmd", mock_exec_cmd)
+        mocker.patch("host.scripts.dss_host.exec_cmd_remote",
+                     mock_exec_cmd_remote)
+        res = discover_dist(1234, ["1234"], ["1234"], "pw")
+        assert len(res) > 0
+
+    def test_config_minio_dist(self, mocker):
+        mocker.patch("builtins.open")
+        mocker.patch("os.chmod")
+        config_minio_dist(["ip", "port", "dev_start", "dev"], 1)
+
+    def test_config_minio_sa(self, mocker):
+        mocker.patch("builtins.open")
+        config_minio_sa(["ip", "port", "dev_start", "dev"], 1)
+
+    def test_dss_host_args_config_host(self, mocker):
+        mocker.patch("argparse.ArgumentParser", MockArgParser)
+        mocker.patch("host.scripts.dss_host.exec_cmd", mock_exec_cmd)
+        mocker.patch("host.scripts.dss_host.exec_cmd_remote",
+                     mock_exec_cmd_remote)
+        mocker.patch("builtins.open")
+        mocker.patch("os.chmod")
+        mocker.patch("os.chdir")
+        mocker.patch("os.path.exists")
+        mocker.patch("json.load")
+        mocker.patch("json.dump")
+        d = dss_host_args()
---
+++ unittests/test_dss_target.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+
+"""
+# The Clear BSD License
+#
+# Copyright (c) 2023 Samsung Electronics Co., Ltd.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted (subject to the limitations in the disclaimer
+# below) provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Samsung Electronics Co., Ltd. nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+# THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from target.dss_target import (
+    random_with_N_digits, generate_core_mask, generate_core_mask_vmmode,
+    get_pcie_address_firmware_mapping, get_pcie_address_serial_mapping,
+    get_nvme_list_numa, ip4_addresses, only_mtu9k, get_rdma_ips,
+    get_numa_boundary, get_numa_ip, create_nvmf_config_file, get_vlan_ips,
+    setup_hugepage
+)
+import pytest
+from conftest import (
+    mock_exec_cmd, mock_os_dirname_host,
+    mock_exec_cmd_remote,
+    MockArgParser, MockArgs,
+    mock_interfaces, mock_ifaddresses
+)
+
+
+class TestDSSTarget():
+    def test_random_with_N_digits(self):
+        num_digits = 10
+        num = random_with_N_digits(10)
+        assert len(str(num)) == num_digits
+
+    def test_generate_core_mask(self):
+        core_count = 12
+        dedicated_core_percent = 0.6
+        return 0 == generate_core_mask(core_count, dedicated_core_percent)
+
+    def test_generate_core_mask_vmmode(self):
+        core_count = 12
+        return 0 == generate_core_mask_vmmode(core_count)
+
+    def test_get_pcie_address_firmware_mapping(self, mocker):
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        mocker.patch("builtins.open")
+        mocker.patch("typing.IO.readlines")
+        r1, r2 = get_pcie_address_firmware_mapping()
+
+    def test_get_pcie_address_serial_mapping(self, mocker):
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        mocker.patch("builtins.open")
+        mocker.patch("typing.IO.readlines")
+        r = get_pcie_address_serial_mapping([])
+        assert len(r) > 0
+
+    def test_get_nvme_list_numa(self, mocker):
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        r = get_nvme_list_numa()
+        assert len(r) > 0
+
+    def test_ip4_addresses(self, mocker):
+        r = ip4_addresses()
+        assert len(r) > 0
+
+    def test_only_mtu9k(self, mocker):
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        r = only_mtu9k({"ip": 100})
+        assert len(r) > 0
+
+    def test_get_rdma_ips(self, mocker):
+        r = get_rdma_ips({"nic": "1.2.3.4"}, ["1.2.3.4"])
+        assert len(r) > 0
+
+    def test_get_numa_boundary(self, mocker):
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        r = get_numa_boundary()
+        assert r == 1.0
+
+    def test_get_numa_ip(self, mocker):
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        mocker.patch("target.dss_target.interfaces", mock_interfaces)
+        mocker.patch("target.dss_target.ifaddresses", mock_ifaddresses)
+        mocker.patch("target.dss_target.AF_INET", 0)
+        r1, r2 = get_numa_ip(["1.2.3.4"])
+        assert len(r1) > 0
+        assert len(r2) == 0
+
+    def test_create_nvmf_config_file(self, mocker):
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        mocker.patch("target.dss_target.interfaces", mock_interfaces)
+        mocker.patch("target.dss_target.ifaddresses", mock_ifaddresses)
+        mocker.patch("target.dss_target.AF_INET", 0)
+        mocker.patch("builtins.open")
+        r = create_nvmf_config_file("", ["1.2.3.4"], [], [])
+        assert r == 0
+
+    def test_get_vlan_ips(self, mocker):
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        r = get_vlan_ips("1234")
+        assert r
+
+    def test_setup_hugepage(self, mocker):
+        mocker.patch("builtins.open")
+        mocker.patch("target.dss_target.exec_cmd", mock_exec_cmd)
+        r = setup_hugepage()
+        assert r == 0
